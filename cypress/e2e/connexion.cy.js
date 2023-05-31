14f51275f007f7d8fa3ab32d6deb9939f3b15058
Tamara Sallerin <tm.sallerin@gmail.com>
2023-05-31 14:08:00 +0200

    upload createAccount

--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -1,33 +1,14 @@
 
 
-
-describe('connexion utilisateur', () => {
-  beforeEach(() => {
-
-    cy.visit('https://opencruise-ok.sogeti-center.cloud/login')
-  })
-
-  it('connexion valide', () => {
-    
-      cy.get('input[formcontrolname="username"]').type('Tam');
-      cy.get('input[formcontrolname="password"]').type('Tam');
-      cy.get('button[class="btn btn-primary"]').click();
-
-  
-  })
-
- 
-  })
-
   describe('connexion utilisateur', () => {
     beforeEach(() => {
   
       cy.visit('https://opencruise-ok.sogeti-center.cloud/login')
     })
   
-    it('connexion invalide', () => {
+    it('connexion valide', () => {
       
-        cy.get('input[formcontrolname="username"]').type('Tom');
+        cy.get('input[formcontrolname="username"]').type('Tam');
         cy.get('input[formcontrolname="password"]').type('Tam');
         cy.get('button[class="btn btn-primary"]').click();
 
@@ -40,3 +21,24 @@ describe('connexion utilisateur', () => {
    
     })
 
+    describe('connexion utilisateur', () => {
+      beforeEach(() => {
+    
+        cy.visit('https://opencruise-ok.sogeti-center.cloud/login')
+      })
+    
+      it('connexion invalide', () => {
+        
+          cy.get('input[formcontrolname="username"]').type('Tom');
+          cy.get('input[formcontrolname="password"]').type('Tam');
+          cy.get('button[class="btn btn-primary"]').click();
+  
+          //verification login
+          cy.url().should('include', 'https://opencruise-ok.sogeti-center.cloud');
+    
+      
+      })
+    
+     
+      })
+      
