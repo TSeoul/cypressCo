c5452b95120464fd8598be44dae5cf3ca6993110
Tamara Sallerin <tm.sallerin@gmail.com>
2023-06-02 12:15:33 +0200

    rm

--- cypress/e2e/page-objects/PageConnexionAdmin.js
+++
@@ -1,7 +0,0 @@
-// Variables connexion admin envPreprod et envTest valide
-
-Cypress.env.loginAdminOk = 'admin@test.com';
-Cypress.env.passwordAdminOk = 'Sogeti33';
-
-Cypress.env.loginAdminKo = 'admin2@test.com';
-Cypress.env.passwordAdminKo = 'Sogeti33'
