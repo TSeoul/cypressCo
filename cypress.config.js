9f652078bd5eff50149d3ce257df12c59f1279d1
Tamara Sallerin <tm.sallerin@gmail.com>
2023-10-12 12:48:22 +0200

    Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -16,26 +16,33 @@ require('dotenv').config();
   
 //});
 
-module.exports = defineConfig({
-  "reporter": "mochawesome",
-  "reporterOptions": {
-    "reportDir": "cypress/results",
-    "overwrite": true,
-    "html": false,
-    "json": true
-    
-  },
+
   
 
-});
+
 module.exports = defineConfig({
   projectId: 'rk1bmu',
-  env: {...process.env},
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions:{
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+
+
+  },
+  env: {
+    ...process.env},
+  
   e2e: {
     setupNodeEvents(on, config) {
-      
-    
+  
       // implement node event listeners here
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+
     },
     
      
