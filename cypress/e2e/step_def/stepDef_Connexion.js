36f30bae54b7644f30fc7c629c1771d8825ff75a
Tamara Sallerin <tm.sallerin@gmail.com>
2023-10-12 10:52:05 +0200

    upload cypress folder

---
+++ cypress/e2e/step_def/stepDef_Connexion.js
@@ -0,0 +1,20 @@
+
+
+// locators connexion utilisateur envPreprod valide
+
+Cypress.env.login = '//input[@formcontrolname="username"]';
+Cypress.env.password = '//input[@formcontrolname="password"]';
+Cypress.env.btnConnexion = '//button[@class="btn btn-primary"]';
+
+
+
+
+
+
+
+
+
+  
+   
+  
+  
---
+++ cypress/e2e/step_def/stepDef_Subscription.js
@@ -0,0 +1,25 @@
+
+//locators formulaire subscription
+
+Cypress.env.radioBtn = '//input[@type="radio"][value="1"]';
+Cypress.env.inputNom = '//input[@formcontrolname="prenom"]';
+Cypress.env.inputPrenom = '//input[@formcontrolname="nom"]';
+Cypress.env.inputDateNaissanceUser = '//input[@formcontrolname="dateNaissance"]';
+Cypress.env.inputAdresse = '//input[@formcontrolname="adresse"]';
+Cypress.env.inputPays = '//select[@formcontrolname="currentPays"]';
+Cypress.env.inputVille = '//select[@formcontrolname="ville"]';
+Cypress.env.inputCP = '//input[@formcontrolname="codePostal"]';
+Cypress.env.inputPassport = '//input[@formcontrolname="passport"]';
+
+Cypress.env.inputTel = '//input[@formcontrolname="tel"]';
+Cypress.env.inputCin = '//input[@formcontrolname="cin"]';
+Cypress.env.inputEmail = '//input[@formcontrolname="username"]';
+Cypress.env.inputPassword = '//input[@formcontrolname="password"]';
+Cypress.env.inputConfirmPassword = '//input[@formcontrolname="confirmPassword"]';
+Cypress.env.inputPrenomConjoint = '//input[@formcontrolname="nomConjoint"]';
+Cypress.env.inputNomConjoint = '//input[@formcontrolname="prenomConjoint"]';
+Cypress.env.inputDateNaissanceConjoint = '//input[@formcontrolname="dateNaissanceConjoint"]';
+Cypress.env.btnValider = '//button[@class="btn btn-primary"]';
+
+Cypress.env.DateNaissanceConjoint = '2002-01-12';
+Cypress.env.DateNaissanceUser = '2002-11-01';
